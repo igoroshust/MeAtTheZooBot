a0e8946148226548f610b1235a210d70cf2b1c81
Igor <igoroshust75@gmail.com>
2025-01-17 21:23:09 +0900

    feat: bug fix

--- MeAtTheZooBot.py
+++ MeAtTheZooBot.py
@@ -1,6 +1,6 @@
 import logging
-import time
 import os
+import time
 
 import telebot
 from telebot import types
@@ -17,135 +17,20 @@ bot = telebot.TeleBot(TOKEN)
 # Абсолютный путь к папке images
 images_folder = os.path.join(os.path.dirname(__file__), 'images')
 
-
-# @bot.message_handler(commands=['start'])
-# def send_welcome(message):
-#     bot.reply_to(message, "Привет! Я ваш бот в зоопарке!")
-#
-# @bot.message_handler(commands=['help'])
-# def send_help(message):
-#     bot.reply_to(message, "Это справка по командам.")
-#
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
-#
-# if __name__ == '__main__':
-#     bot.polling()
-
 users = {}
-
 TELEGRAM_CHAT_ID = 1277852544
 
 animals = [
     {'bear': 'Малайский медведь'},
     {'puma': 'Пума'},
     {'bushdog': 'Кустарниковая собака'},
-    {'penguin': 'Пингвин Гумбольдта'},
+    {'penguin': 'Пингвин Гумбольта'},
     {'parrot': 'Сливоголовый попугай'},
     {'gadwall': 'Серая утка'},
     {'caiman': 'Чёрный кайман'},
     {'iguana': 'Игуана обыкновенная'},
     {'boiga': 'Мангровая змея'},
 ]
-
-class MessageSendingError(Exception):
-    """Ошибка отправки сообщения"""
-    pass
-
-FAILURE_TO_SEND_MESSAGE = '{error}, {message}'
-
-@bot.message_handler(commands=['Save data!'])
-def send_message(message: users):
-    """Отправляет сообщение пользователю в Telegram"""
-    try:
-        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-    except Exception as error:
-        raise MessageSendingError(FAILURE_TO_SEND_MESSAGE.format(
-            error=error,
-            message=message,
-        ))
-    logging.info(f'Сообщение "{message}" доставлено')
-
-@bot.message_handler(commands=["Feedback"])
-def write_to_support(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, 'Введите своё имя')
-    users[chat_id] = {}
-    bot.register_next_step_handler(message, save_username)
-
-def save_username(message):
-    chat_id = message.chat.id
-    name = message.text
-    users[chat_id]['name'] = name
-    bot.send_message(chat_id, f'Хорошо, {name}. Ваше сообщение: ')
-    bot.register_next_step_handler(message, save_surname)
-
-def save_surname(message):
-    chat_id = message.chat.id
-    surname = message.text
-    users[chat_id]['surname'] = surname
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    button_save = telebot.types.InlineKeyboardButton(text="Сохранить", callback_data='save_data')
-    button_change = telebot.types.InlineKeyboardButton(text="Изменить",callback_data='change_data')
-    keyboard.add(button_save, button_change)
-    bot.send_message(chat_id, f'Сохранить данные?', reply_markup=keyboard)
-
-
-@bot.message_handler(commands=['who_i'])
-def who_i(message):
-    chat_id = message.chat.id
-    name = users[chat_id]['name']
-    text = users[chat_id]['text']
-    bot.send_message(chat_id, f'Вы: {name} {text}')
-
-@bot.callback_query_handler(func=lambda call: call.data == 'save_data')
-def save_btn(call):
-    message = call.message
-    chat_id = message.chat.id
-    message_id = message.message_id
-    print(users)
-    bot.edit_message_text(chat_id=chat_id, message_id=message_id,text='Данные сохранены!', reply_markup=send_message(users))
-
-@bot.callback_query_handler(func=lambda call: call.data == 'change_data')
-def save_btn(call):
-    message = call.message
-    chat_id = message.chat.id
-    message_id = message.message_id
-    bot.edit_message_text(chat_id=chat_id, message_id=message_id, text='Изменение данных!')
-    write_to_support(message)
-
-def fine_key(z, key, val):
-    print(key, val, z)
-    time.sleep(2)
-    bot.send_message(chat_id=z, text=f"Поздравляем!\n\n Ваше тотемное животное – {val}!")
-    bot.send_photo(chat_id=z, photo=open(os.path.join(images_folder, f'{key}.JPG'), 'rb'))
-    time.sleep(2)
-    keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="Московский зоопарк", url="https://moscowzoo.ru/animals/")
-    keyboard.add(url_button)
-    bot.send_message(z,
-                     "Взять животное под опеку, а также найти информацию о нём, можно найти на сайте:",
-                     reply_markup=keyboard)
-
-def inline_key(num):
-    """Функция для вывода кнопок"""
-    keyboard = types.InlineKeyboardMarkup()
-    if num == 1:
-        keyboard.add(types.InlineKeyboardButton(text='Малайский медведь', callback_data="bear"))
-        keyboard.add(types.InlineKeyboardButton(text='Пума', callback_data="puma"))
-        keyboard.add(types.InlineKeyboardButton(text='Кустарниковая собака', callback_data="bushdog"))
-    elif num == 2:
-        keyboard.add(types.InlineKeyboardButton(text='Пингвин Гумбольдта', callback_data="penguin"))
-        keyboard.add(types.InlineKeyboardButton(text='Сливоголовый попугай', callback_data="parrot"))
-        keyboard.add(types.InlineKeyboardButton(text='Серая утка', callback_data="gadwall"))
-    elif num == 3:
-        keyboard.add(types.InlineKeyboardButton(text='Чёрный кайман', callback_data="caiman"))
-        keyboard.add(types.InlineKeyboardButton(text='Игуана обыкновенная', callback_data="iguana"))
-        keyboard.add(types.InlineKeyboardButton(text='Мангровая змея', callback_data="boiga"))
-
-    return keyboard
-
 
 @bot.message_handler(commands=["start"])
 def start(m):
@@ -164,10 +49,11 @@ def start(m):
     # Отправляем текстовое сообщение
     bot.send_message(chat_id, text, reply_markup=key)
 
-
 @bot.callback_query_handler(func=lambda call: call.data == 'pick_animal')
 def handle_pick_animal(call):
     chat_id = call.message.chat.id
+    bot.send_message(chat_id, "Сейчас мы покажем вам список животных")
+    time.sleep(1)
 
     for dic in animals:  # выводит список словарей
         for key, val in dic.items():
@@ -176,7 +62,7 @@ def handle_pick_animal(call):
                 bot.send_photo(chat_id, img)
             time.sleep(1)
 
-    # Создаём клавиатуру с кнопкой выбора категории животного
+    # Создаём клавиатуру с кнопками выбора категории животного
     key = types.InlineKeyboardMarkup()
     key.add(types.InlineKeyboardButton(text='Млекопитающие', callback_data="mammals"))
     key.add(types.InlineKeyboardButton(text='Птицы', callback_data="birds"))
@@ -184,8 +70,6 @@ def handle_pick_animal(call):
 
     # Отправляем финальное сообщение
     bot.send_message(chat_id, 'Какие животные Вам больше нравятся?', reply_markup=key)
-    logging.info(chat_id)
-
 
 @bot.callback_query_handler(func=lambda call: True)
 def inline(c):
@@ -210,13 +94,10 @@ def inline(c):
             text="Какая рептилия Вам больше понравилась?",
             parse_mode="markdown",
             reply_markup=inline_key(3))
-        print(c.data)
     elif c.data in ['bear', 'puma', 'bushdog', 'penguin', 'parrot', 'gadwall', 'caiman', 'iguana', 'boiga']:
         for dic in animals:  # выводит список словарей
             for key, val in dic.items():
-                # print(f'{key} is {val}')
                 if c.data == key:
-                    # print(c.data, "*********")
                     bot.answer_callback_query(callback_query_id=c.id, text='! ! ! Ура ! ! !')
                     bot.edit_message_text(
                         chat_id=c.message.chat.id,
@@ -224,20 +105,87 @@ def inline(c):
                         text=f"123",
                         parse_mode="markdown",
                         reply_markup=fine_key(c.message.chat.id, key, val))
-        time.sleep(2)
-        bot.send_message(c.message.chat.id, text=f"\nНе устраивает результат? \nПройдите тест ещё раз - /start\
-                            \nКонтакт - igoroshust@yandex.ru\nОбратная связь - /Feedback")
 
 
-# Обрабатывается все документы и аудиозаписи
-@bot.message_handler(content_types=['photo', 'document', 'audio'])
-def say_lmao(message: telebot.types.Message):
-    bot.reply_to(message, 'Nice meme XDD')
+def fine_key(z, key, val):
+    print(key, val, z)
+    bot.send_message(chat_id=z, text=f"Поздравляем!\n\n Ваше тотемное животное – {val}!")
+    with open(os.path.join(images_folder, f'{key}.JPG'), 'rb') as img:
+        bot.send_photo(chat_id=z, photo=img)
+    time.sleep(1)
+
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Московский зоопарк", url="https://moscowzoo.ru/animals/")
+    keyboard.add(url_button)
+    bot.send_message(z,
+                     "Взять животное под опеку, а также найти информацию о нём, можно найти на сайте:",
+                     reply_markup=keyboard)
+
+
+def inline_key(num):
+    """Функция для вывода кнопок"""
+    keyboard = types.InlineKeyboardMarkup()
+    if num == 1:
+        keyboard.add(types.InlineKeyboardButton(text='Малайский медведь', callback_data="bear"))
+        keyboard.add(types.InlineKeyboardButton(text='Пума', callback_data="puma"))
+        keyboard.add(types.InlineKeyboardButton(text='Кустарниковая собака', callback_data="bushdog"))
+    elif num == 2:
+        keyboard.add(types.InlineKeyboardButton(text='Пингвин Гумбольта', callback_data="penguin"))
+        keyboard.add(types.InlineKeyboardButton(text='Сливоголовый попугай', callback_data="parrot"))
+        keyboard.add(types.InlineKeyboardButton(text='Серая утка', callback_data="gadwall"))
+    elif num == 3:
+        keyboard.add(types.InlineKeyboardButton(text='Чёрный кайман', callback_data="caiman"))
+        keyboard.add(types.InlineKeyboardButton(text='Игуана обыкновенная', callback_data="iguana"))
+        keyboard.add(types.InlineKeyboardButton(text='Мангровая змея', callback_data="boiga"))
+
+    return keyboard
+
+
+@bot.message_handler(commands=["Feedback"])
+def write_to_support(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Введите своё имя')
+    users[chat_id] = {}
+    bot.register_next_step_handler(message, save_username)
+
+
+def save_username(message):
+    chat_id = message.chat.id
+    name = message.text
+    users[chat_id]['name'] = name
+    bot.send_message(chat_id, f'Хорошо, {name}. Ваше сообщение: ')
+    bot.register_next_step_handler(message, save_surname)
+
+
+def save_surname(message):
+    chat_id = message.chat.id
+    surname = message.text
+    users[chat_id]['surname'] = surname
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button_save = telebot.types.InlineKeyboardButton(text="Сохранить", callback_data='save_data')
+    button_change = telebot.types.InlineKeyboardButton(text="Изменить", callback_data='change_data')
+    keyboard.add(button_save, button_change)
+    bot.send_message(chat_id, f'Сохранить данные?', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'save_data')
+def save_btn(call):
+    message = call.message
+    chat_id = message.chat.id
+    bot.edit_message_text(chat_id=chat_id, message_id=message.message_id,
+                          text='Я отправил Вашу обратную связь сотрудникам зоопарка, ожидайте обратной связи!')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'change_data')
+def change_btn(call):
+    message = call.message
+    chat_id = message.chat.id
+    bot.edit_message_text(chat_id=chat_id, message_id=message.message_id, text='Изменение данных!')
+    write_to_support(message)
 
 
 @bot.message_handler(commands=['help', 'information'])
 def send_help(message):
-    print(message.text)  # получить команду
     text = ('Московский зоопарк — один из старейших зоопарков Европы с уникальной коллекцией животных и профессиональным\
      сообществом. Он выполняет много функций, например, там проводятся разные лекции для посетителей и курсы \
      профессиональной переподготовки для специалистов сферы. \n \n \
@@ -245,17 +193,18 @@ def send_help(message):
 «Возьми животное под опеку» («Клуб друзей») — это одна из программ, помогающих зоопарку заботиться о его обитателях. \
 Программа позволяет с помощью пожертвования на любую сумму внести свой вклад в развитие зоопарка и сохранение \
 биоразнообразия планеты.\n \
-\tВзять под опеку можно разных обитателей зоопарка, например, слона, льва, суриката или фламинго. Это возможность помочь \
-любимому животному или даже реализовать детскую мечту подружиться с настоящим диким зверем. Почётный статус опекуна \
-позволяет круглый год навещать подопечного, быть в курсе событий его жизни и самочувствия.\
-переподготовки для специалистов сферы.\n \
-\tУчастником программы может стать любой неравнодушный: и ребёнок, и большая корпорация. Поддержка опекунов \
-помогает зоопарку улучшать условия для животных и повышать уровень их благополучия.\n \
-Контакт - igoroshust@yandex.ru\n Обратная связь - /Feedback \n Подобрать подопечного - /start')
+\tВзять под опеку можно разных обитателей зоопарка, например, слона, льва, суриката или фламинго. Это возможность помочь любимому животному или даже реализовать детскую мечту подружиться с настоящим диким зверем. Почётный статус опекуна \
+    позволяет круглый год навещать подопечного, быть в курсе событий его жизни и самочувствия.\n \
+    Участником программы может стать любой неравнодушный: и ребёнок, и большая корпорация. Поддержка опекунов \
+    помогает зоопарку улучшать условия для животных и повышать уровень их благополучия.\n \
+    Контакт - igoroshust@yandex.ru\n Обратная связь - /Feedback \n Подобрать подопечного - /start')
     bot.send_message(message.chat.id, text)
 
+# Обрабатывается все документы и аудиозаписи
+@bot.message_handler(content_types=['photo', 'document', 'audio'])
+def say_lmao(message: telebot.types.Message):
+    bot.reply_to(message, 'Nice meme XDD')
 
 if __name__ == '__main__':
     print('Бот запущен!')
-
     bot.polling(none_stop=True)
